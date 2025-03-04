f9dbb71de44770a68028bae91ff2ae0360240b92
ShubhamSChede <shubhamchede1602@gmail.com>
2025-03-04 20:26:02 +0530

    10th commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import ProjectSection from '../components/ProjectSection';
 import AboutMe from '../components/AboutMe';
 import TechStack from '../components/TechStack';
@@ -7,29 +7,13 @@ import Navbar from '../components/Navbar';
 import ContactMe from '../components/ContactMe';
 
 const Page = () => {
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    // Reduce video playback speed by 50%
-    if (videoRef.current) {
-      videoRef.current.playbackRate = 0.4;
-    }
-  }, []);
-
   return (
     <div className="relative w-full">
-      {/* Background Video */}
-      <video
-        ref={videoRef}
-        className="fixed top-0 left-0 w-full h-full object-cover -z-10 opacity-20"
-        autoPlay
-        loop
-        muted
-        playsInline
-      >
-        <source src="/videos/backgroundspace.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      {/* Background Image */}
+      <div 
+        className="fixed top-0 left-0 w-full h-full -z-10 opacity-20 bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: 'url(/images/bg.jpg)' }}
+      />
 
       {/* Navigation */}
       <Navbar />
@@ -46,7 +30,7 @@ const Page = () => {
           <ProjectSection />
         </div>
         <div id="contact">
-        <ContactMe />
+          <ContactMe />
         </div>
       </div>
     </div>
